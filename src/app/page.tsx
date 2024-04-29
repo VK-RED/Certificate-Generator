f2588d01c57857f571059bcf8dd282eb2e2e2d14
VK-RED <vishnukumart96@gmail.com>
2024-04-29 20:57:18 +0530

    Style HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { BioDataCard } from "@/components/BioDataCard";
 import { TitleCard } from "@/components/TitleCard";
+import { ToggleTab } from "@/components/ToggleTab";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <TitleCard />
-      <BioDataCard/>
+      <ToggleTab/>
       <BackgroundBeams />
     </div>
   )
