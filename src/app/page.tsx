ba7615f5bf9a5d55e4565d59205925e15eb6c096
VK-RED <vishnukumart96@gmail.com>
2024-04-29 20:24:04 +0530

    Style HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { BioDataCard } from "@/components/BioDataCard";
+import { TitleCard } from "@/components/TitleCard";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
 export default function Home() {
   return (
-    <div>
-      Hi
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <TitleCard />
+      <BioDataCard/>
+      <BackgroundBeams />
     </div>
   )
 }
