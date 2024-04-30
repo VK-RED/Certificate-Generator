5c0ee1f13a440d7648561f8b360abfd9e90750d1
VK-RED <vishnukumart96@gmail.com>
2024-04-30 14:24:34 +0530

    Put all providers inside of Providers Component

---
+++ src/components/providers.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import { Toaster } from "./ui/toaster"
+import { RecoilRoot } from "recoil";
+
+export function Providers({ children }: ThemeProviderProps) {
+  return (
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <RecoilRoot>
+          {children}
+        </RecoilRoot>
+        <Toaster />
+      </NextThemesProvider>
+  )
+}
