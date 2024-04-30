15d4ed3b220167aa1609d03ee44e04529a7d10bd
VK-RED <vishnukumart96@gmail.com>
2024-04-30 12:17:23 +0530

    Create Validation and Types for CreateCert

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import z from 'zod'
+
 export interface GenerateCertProps{
     userName:string,
     certificateId: string,
@@ -6,3 +8,10 @@ export interface GenerateCertProps{
 export interface CertificateRendererProps{
     pdfDataUri: string,
 }
+
+export const CreateCert = z.object({
+    name: z.string().min(3,"Name should have atleast 3 letters").max(25, "Name can be atmost 25 letters"),
+    email: z.string().min(11,"Email should be atleast 11 letters").max(40, "Email can be atmost 40 letters")
+})
+
+export type CreateCertProps = z.infer<typeof CreateCert>;
