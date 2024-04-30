aa4afd9d087b505d8562be70b26e4bd7b1afbf87
VK-RED <vishnukumart96@gmail.com>
2024-04-30 21:55:52 +0530

    Cleanup Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { TitleCard } from "@/components/TitleCard";
 import { HeroCard } from "@/components/heroCard";
+import { Marketing } from "@/components/marketing";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <Marketing/>
       <TitleCard />
       <HeroCard/>
       <BackgroundBeams />
