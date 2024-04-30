de455f371df2455e04cc780cfa1cfa0cb559847b
VK-RED <vishnukumart96@gmail.com>
2024-04-30 13:49:50 +0530

    Create Common Messages file

---
+++ src/lib/messages.ts
@@ -0,0 +1,5 @@
+export const NO_CERT_FOUND = "No Certificate found !! Kindly create one !";
+export const SOMETHING_WENT_WRONG = "Something Went Wrong !! Please Contact admin";
+export const CERTID_OR_EMAIL = "Enter Certificate ID or Email !";
+export const CERT_FOUND = "Certificate found for this Email !";
+export const CERT_CREATED = "Certificate Created Successfully !";
