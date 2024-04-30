9c7f00c431527d83ba50eb62234c7a542e18bb37
VK-RED <vishnukumart96@gmail.com>
2024-04-30 21:00:01 +0530

    Add HeroCard in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { TitleCard } from "@/components/TitleCard";
-import { ToggleTab } from "@/components/ToggleTab";
+import { HeroCard } from "@/components/heroCard";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <TitleCard />
-      <ToggleTab/>
+      <HeroCard/>
       <BackgroundBeams />
     </div>
   )
