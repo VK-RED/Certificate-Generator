3dac4b91563bf13e383d86a69e70e70967785789
VK-RED <vishnukumart96@gmail.com>
2024-04-30 13:51:48 +0530

    Style components

--- src/components/BioDataCard.tsx
+++ src/components/BioDataCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     Card,
     CardContent,
@@ -9,9 +11,53 @@ import {
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import { Label } from "./ui/label"
+import { useState } from "react"
+import { createCert } from "@/actions/createCert"
+import { useToast } from "./ui/use-toast"
+import { CERT_CREATED, CERT_FOUND } from "@/lib/messages"
   
 
 export const BioDataCard = () =>{
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [loading, setLoading] = useState(false);
+    const {toast} = useToast();
+
+    const handleSubmit = async () => {
+        setLoading(true);
+
+        if(!name || !email || !isValidEmail()){
+            toast({
+                title:"Enter Valid Name & Email",
+                variant:"destructive"
+            })
+            setLoading(false);
+            return;
+        }
+        
+        const certDetails = await createCert({email,name:name.toLowerCase()});
+        setLoading(false);
+
+        if(certDetails.message == CERT_CREATED ||certDetails.message == CERT_FOUND){
+            toast({title:certDetails.message});
+            setEmail("");
+            setName("");
+        }
+        else{
+            toast({
+                title:certDetails.message,
+                variant:"destructive"
+            })
+        }
+        
+        console.log(certDetails);
+    }
+
+    function isValidEmail():(boolean) {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
     return (
         <Card className="z-10 bg-opacity-100 border-slate-700 min-w-[400px]">
             <CardHeader className="space-y-1">
@@ -23,15 +69,21 @@ export const BioDataCard = () =>{
             <CardContent className="grid gap-4">
                 <div className="grid gap-2">
                     <Label htmlFor="name">Name</Label>
-                    <Input id="name" type="text" placeholder="Adithiyan"/>
+                    <Input id="name" type="text" placeholder="Adithiyan" 
+                            value={name} 
+                            onChange={(e)=>setName((p)=>e.target.value)} 
+                    />
                 </div>
                 <div className="grid gap-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="adithiyan@example.com" />
+                    <Input id="email" type="email" placeholder="adithiyan@example.com" 
+                            value={email}
+                            onChange={(e)=>setEmail((p)=>e.target.value)}
+                    />
                 </div>
             </CardContent>
             <CardFooter>
-                <Button className="w-full">Create</Button>
+                <Button disabled={loading} onClick={handleSubmit} className="w-full">Create</Button>
             </CardFooter>
         </Card>
 
--- src/components/VerifyCard.tsx
+++ src/components/VerifyCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     Card,
     CardContent,
@@ -9,8 +11,65 @@ import {
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import { Label } from "./ui/label"
+import { useState } from "react"
+import { verifyCert } from "@/actions/verify";
+import { useToast } from "./ui/use-toast";
+import { CERT_FOUND, NO_CERT_FOUND } from "@/lib/messages";
 
 export const VerifyCard = () => {
+
+    const [certId,setCertId] = useState("");
+    const [email, setEmail] = useState("");
+    const [loading,setLoading] = useState(false);
+    const {toast} = useToast();
+
+    function isValidEmail():(boolean) {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const handleSubmit = async()=>{
+        setLoading(true);
+
+        if(!certId && !email ){
+            toast({
+                title:"Both fields can't be empty",
+                variant:"destructive"
+            })
+            setLoading(false);
+            return;
+        }
+        else if(!isValidEmail()){
+            toast({
+                title:"Enter valid Email",
+                variant:"destructive"
+            })
+            setLoading(false);
+            return;
+        }
+
+        const certDetails = await verifyCert({certId:certId,userEmail:email});
+        console.log(certDetails);
+
+        if(certDetails.message){
+            if(certDetails.message === NO_CERT_FOUND ||certDetails.message ===  CERT_FOUND){
+                toast({title:certDetails.message});
+            }
+            else{
+                toast({
+                    title:certDetails.message,
+                    variant:"destructive"
+                })
+            }
+        }
+
+        setCertId("");
+        setEmail("");
+
+        setLoading(false);
+        return;
+    }
+
     return (
         <Card className="z-10 bg-opacity-100 border-slate-700 min-w-[400px]">
             <CardHeader className="space-y-1">
@@ -21,12 +80,30 @@ export const VerifyCard = () => {
             </CardHeader>
             <CardContent className="grid gap-4">
                 <div className="grid gap-2">
-                    <Label htmlFor="name">{`Certificate ID`}</Label>
-                    <Input id="name" type="text" placeholder="4rnjkn3"/>
+                    <Label htmlFor="certid">{`Certificate ID`}</Label>
+                    <Input value={certId} onChange={(e)=>setCertId((p)=>e.target.value)}
+                            id="certid" type="text" placeholder="4rnjkn3"/>
+                </div>
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t" />
+                    </div>
+                    <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-background px-2 text-muted-foreground">
+                            or
+                        </span>
+                    </div>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="email">{`Email`}</Label>
+                    <Input id="email" type="email" placeholder="adithiyan@example.com" 
+                            value={email}
+                            onChange={(e)=>setEmail((p)=>e.target.value)}
+                    />
                 </div>
             </CardContent>
             <CardFooter>
-                <Button className="w-full">Create</Button>
+                <Button disabled={loading} onClick={handleSubmit} className="w-full">Verify</Button>
             </CardFooter>
         </Card>
     )
