7a38fdcbfa4673569c3ef776a0bcbb68bf354b02
VK-RED <vishnukumart96@gmail.com>
2024-04-30 21:53:46 +0530

    Modify Header Text

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,7 +1,7 @@
 export const TitleCard = () => {
     return (
         <h1 className="relative z-10 text-lg md:text-6xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          {`Generate Certificate in Seconds ..`}
+          {`Cert-Gen`}
         </h1>
     )
 }
