26c524bb2e10658ee2969813eb2990892bcfb4e5
VK-RED <vishnukumart96@gmail.com>
2024-05-01 12:58:25 +0530

    Add ButtonRows in HeroCard

--- src/components/heroCard.tsx
+++ src/components/heroCard.tsx
@@ -1,11 +1,15 @@
+import { ButtonRows } from "./ButtonRows"
 import { CertRendererClient } from "./CertificateRendererClient"
 import { ToggleTab } from "./ToggleTab"
 
 export const HeroCard = () => {
     return (
-        <div className="z-10 mt-10 flex justify-center items-center space-x-4">
-            <ToggleTab/>
-            <CertRendererClient/>
+        <div className="z-10 mt-10">
+            <div className="flex justify-center items-center space-x-4 z-10">
+                <ToggleTab/>
+                <CertRendererClient/>
+            </div>
+            <ButtonRows className="mt-5"/>
         </div>
     )
 }
