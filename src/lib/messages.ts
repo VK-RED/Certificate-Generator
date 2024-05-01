e6ac0b1504937d1acb1faaf2dd8299f8df845851
VK-RED <vishnukumart96@gmail.com>
2024-05-01 12:43:34 +0530

    create SHARE_MESSAGE

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -3,3 +3,4 @@ export const SOMETHING_WENT_WRONG = "Something Went Wrong !! Please Contact admi
 export const CERTID_OR_EMAIL = "Enter Certificate ID or Email !";
 export const CERT_FOUND = "Certificate found for this Email !";
 export const CERT_CREATED = "Certificate Created Successfully !";
+export const SHARE_MESSAGE = `Hello all, I am very excited to share that I have successfully completed the Fullstack Live 0-1 Cohort and learned things from Kirat !! #certification #100xDevs 🎓 🏆 `
