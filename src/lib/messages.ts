0e1467db88e8b24732e86ebcc46be2607d09129b
VK-RED <vishnukumart96@gmail.com>
2024-05-02 12:52:15 +0530

    Add Cert Messages

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -4,3 +4,5 @@ export const CERTID_OR_EMAIL = "Enter Certificate ID or Email !";
 export const CERT_FOUND = "Certificate found for this Email !";
 export const CERT_CREATED = "Certificate Created Successfully !";
 export const SHARE_MESSAGE = `Hello all, I am very excited to share that I have successfully completed the Fullstack Live 0-1 Cohort and learned things from Kirat !! #certification #100xDevs 🎓 🏆 `
+export const NO_TEMPLATE_FOUND = `Template Certificate Not found !!`
+export const CERT_GEN_ERROR = "Error in Generating the PDF, try again later !"
