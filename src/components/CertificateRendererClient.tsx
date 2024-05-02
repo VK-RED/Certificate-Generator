16e83a45142534d8c4f3e63948c284c3ee926ff8
VK-RED <vishnukumart96@gmail.com>
2024-05-02 12:53:16 +0530

    fix types in CertificateRendererClient Component

--- src/components/CertificateRendererClient.tsx
+++ src/components/CertificateRendererClient.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { certSelector } from "@/store/selectors/cert";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { CertificateRenderer } from "./CertificateRender";
 import { generateCert } from "@/actions/cert";
 import { certAtom } from "@/store/atoms/cert";
+import { useToast } from "./ui/use-toast";
+import { CERT_GEN_ERROR } from "@/lib/messages";
 
 export const CertRendererClient = () => {
     const cert = useRecoilValue(certSelector);
     const setCert = useSetRecoilState(certAtom);
+    const {toast} = useToast();
 
     useEffect(()=>{
         if(!cert.pdfDataUri){
@@ -22,6 +25,10 @@ export const CertRendererClient = () => {
             return;
         }
         const data = await generateCert({userName:cert.name, certificateId:cert.id});
+        if(!data.pdfUri){
+            toast({title:CERT_GEN_ERROR})
+            return;
+        }
         setCert(p=>({...p,pdfDataUri:data.pdfUri}));
         console.log("Cert from store is ", cert);
     }
